f275370bcfc6a5219ef368a11ccf02022ea715e7
hitman802 <dadaetoimennoon@gmail.com>
2017-07-19 14:22:32 +0300

    change spring-boot version due to bug with jsp and js resources https://github.com/spring-projects/spring-boot/issues/8986

--- src/main/java/initializer/AppInitializer.java
+++ src/main/java/initializer/AppInitializer.java
@@ -5,8 +5,6 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
