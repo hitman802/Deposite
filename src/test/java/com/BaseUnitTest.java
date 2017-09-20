37fc02757081da5643795637fa5c15064a7d1a44
hitman802 <dadaetoimennoon@gmail.com>
2017-09-20 17:34:02 +0300

    fixed logger conflict fixed abstract class for testng

--- src/test/java/com/BaseUnitTest.java
+++ src/test/java/com/BaseUnitTest.java
@@ -1,12 +1,14 @@
 package com;
 
 import com.periodical.RatesUpdater;
+import lombok.extern.log4j.Log4j;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.IObjectFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.ObjectFactory;
 
-public abstract class BaseUnitTest extends AbstractTestNGSpringContextTests {
+@Log4j
+public class BaseUnitTest extends AbstractTestNGSpringContextTests {
     /*
     * for power mock working
     */
@@ -15,7 +17,9 @@ public abstract class BaseUnitTest extends AbstractTestNGSpringContextTests {
         return new org.powermock.modules.testng.PowerMockObjectFactory();
     }
 
-    public abstract void prepareMocks();
+    public void prepareMocks() {
+        log.warn("Prepare mock method not overridden");
+    };
 
     @BeforeMethod
     public void setUp() throws Exception {
